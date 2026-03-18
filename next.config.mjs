6512e1bc5a1c3dff050153f9fcc8f224be32de1f
IncuwiseManan <manan@incuwise.com>
2026-03-18 15:04:08 +0530

    json files

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [{ key: 'Content-Type', value: 'application/json' }],
+      },
+    ];
+  },
   async redirects() {
     return [
       { source: '/qr', destination: '/', permanent: true },
